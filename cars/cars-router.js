1735fc8473ec618b8067730e0e325c6f850db36b
Stephen Gary <sgary0@protonmail.com>
2020-08-11 16:04:55 -0400

    testing

---
+++ cars/cars-router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const knex = require('knex');
+
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: './data/cars-dealer.db3'
+    },
+    useNullAsDefault: true
+});
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db('cars')
+  .then(cars => {
+      res.status(200).json(cars);
+  })
+  .catch(error => {
+      console.log(error);
+      res.status(500).json({
+          message: error.message
+      });
+  });  
+});
+
+router.post('/', (req, res) => {
+    const carsData = req.body;
+    db('cars').insert(carsData)
+    .then(ids => {
+        db('cars').where({ id: ids[0] })
+        .then(newCarEntry => {
+            res.status(200).json(newCarEntry);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    });
+});
+
+module.exports = router;
