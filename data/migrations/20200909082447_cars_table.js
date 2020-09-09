2cc52ec35ee862be2f0c61cea7112aed2581612c
Stephen Gary <sgary0@protonmail.com>
2020-09-09 08:26:19 -0400

    updated migrations

---
+++ data/migrations/20200909082447_cars_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments('id');
+        tbl.string('VIN').notNullable().unique();
+        tbl.string('make').notNullable();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('transmission');
+        tbl.string('status');
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+  };
+  
