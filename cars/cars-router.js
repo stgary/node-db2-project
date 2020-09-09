d7db2ae374371c7935cad62c2094ac43f3b7acd3
Stephen Gary <sgary0@protonmail.com>
2020-09-09 08:23:07 -0400

    updated cars-router

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const knex = require('knex');
-
-const db = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: './data/cars-dealer.db3'
-    },
-    useNullAsDefault: true
-});
-
+const db = require('../data/dbConfig.js');
 const router = express.Router();
+
+// const knex = require('knex');
+// const db = knex({
+//     client: 'sqlite3',
+//     connection: {
+//         filename: './data/cars-dealer.db3'
+//     },
+//     useNullAsDefault: true
+// });
 
 router.get('/', (req, res) => {
   db('cars')
@@ -25,21 +25,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const carsData = req.body;
-    db('cars')
-        .insert(carsData)
-        .then(ids => {
-            db('cars').where({ id: ids[0] })
-            .then(newCarEntry => {
-                res.status(200).json(newCarEntry);
-            });
+  const carsData = req.body;
+  db('cars')
+    .insert(carsData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+        .then(newCarEntry => {
+            res.status(200).json(newCarEntry);
+          });
         })
         .catch(error => {
-            console.log(error);
+          console.log(error);
             res.status(500).json({
-                message: error.message
-            });
-        });
+              message: error.message
+          });
+      });
 });
 
 module.exports = router;
